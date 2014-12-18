8194c4aaf6d757bd50f6d9a1f7f3dcb8e297e9e0
JonnoFTW <jonmac1@gmail.com>
2014-12-18 15:23:13 +1030

    Updated sudoku solver to use simulated annealing. Should probably use the candidates list properly, but I don't think it works right now.

--- src/Prob96.java
+++ src/Prob96.java
@@ -1,54 +1,39 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Arrays;
-import java.util.BitSet;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.apache.commons.lang3.ArrayUtils;
 
 public class Prob96 {
 	private static Random rand = new Random();
-
-	private static boolean hasDuplicates(final int[] arr) {
-		final int MAXZIP = 99999;
-
-		BitSet b = new BitSet(MAXZIP + 1);
-		b.set(0, MAXZIP, false);
-		for (int item : arr) {
-			if (!b.get(item)) {
-				b.set(item, true);
-			} else
-				return true;
-		}
-		return false;
-	}
-
+	private static AtomicInteger sum = new AtomicInteger(0);
 	private static int countErrors(int[][] board) {
 		int count = 0;
-		// check rows
+		// check rows and columns for errors only
 		for (int i = 0; i < board.length; i++) {
 			if(!validRow(board[i]))
 				count++;
 			if (!validRow(arrayColumn(board, i)))
 				count++;
-		}	
-		/*
-		 * // check boxes for (int i = 0; i < 3; i++) { for (int j = 0; j < 3;
-		 * j++) { if (hasDuplicates(quad(board, i, j))) count++; } }
-		 */
+		}
 		return count;
 	}
 
 	private static boolean validRow(int[] row) {
-		int sum=0,product=0;
+		int s=0,product=1;
 		for (int j = 0; j < row.length; j++) {
-			sum += row[j];
+			s += row[j];
 			product *= row[j];
 		}
-		return sum == 45 && product ==362880;
+		return s == 45 && product == 362880;
 	}
 
 	private static int[] arrayColumn(final int[][] arr, final int col) {
@@ -75,17 +60,14 @@ public class Prob96 {
 			newSolution[x * 3 + i / 3][y * 3 + i % 3] = quad[i];
 		}
 	}
-
-	private static double acceptanceProbability(double currentEnergy,
-			double neighbourEnergy, double temperature) {
-		// If the new solution is better, accept it
-		if (neighbourEnergy < currentEnergy) {
-			return 1.0;
+	
+	private static int count(int[] arr, int needle) {
+		int count=0;
+		for (int i = 0; i < arr.length; i++) {
+			if(arr[i] == needle)count++;
 		}
-		// If the new solution is worse, calculate an acceptance probability
-		return Math.exp(-((neighbourEnergy - currentEnergy) / temperature));
+		return count;
 	}
-
 	/***
 	 * Initialise so that all 3x3 subgrids contains [1..9]
 	 * 
@@ -102,15 +84,46 @@ public class Prob96 {
 		for (int i = 0; i < 3; i++) {
 			for (int j = 0; j < 3; j++) {
 				int[] quad = quad(newSolution, i, j);
+				int zeroes = count(quad,0);
 				for (int c = 1; c <= 9; c++) {
 					if (!ArrayUtils.contains(quad, c)) {
 						quad[ArrayUtils.indexOf(quad, 0)] = c;
 					}
 				}
+				if(zeroes == 1) {
+					setQuad(quad, board, i, j);
+				}
 				setQuad(quad, newSolution, i, j);
 			}
 		}
 		return newSolution;
+	}
+	private static int[][][] candidates(final int[][] board) {
+		int[][][] cand = new int[9][9][10];
+		while(true) {
+			int solved = 0;
+			for (int i = 0; i < board.length; i++) {
+				for (int j = 0; j < board[i].length; j++) {
+					if(board[i][j] == 0) {
+						int[] row = board[i];
+						int[] col = arrayColumn(board, j);
+						int[] qad = quad(board,i/3,j/3);
+						for (int k = 1; k < cand[i][j].length; k++) {
+							if(ArrayUtils.contains(qad, k) || ArrayUtils.contains(col, k) || ArrayUtils.contains(row,k))
+								cand[i][j][k]=1;
+						}
+						if(count(cand[i][j],0) == 2) {
+							board[i][j] = ArrayUtils.lastIndexOf(cand[i][j], 0);
+							solved++;
+						}
+					}
+				}
+			}
+			if(solved ==0) {
+				break;
+			}
+		}
+		return cand;
 	}
 
 	private static LinkedList<Integer> indexesOf(int[] arr, int needle) {
@@ -121,7 +134,6 @@ public class Prob96 {
 			}
 		}
 		return l;
-
 	}
 
 	/**
@@ -131,65 +143,114 @@ public class Prob96 {
 	 * @param x
 	 * @param y
 	 */
-	private static void swap(int[][] arr, int[][] board, int x, int y) {
+	private static void swap(int[][] arr, int[][] board,int[][][] cands, int x, int y) {
 		int[] quad = quad(board, x, y);
 		int[] quadNew = quad(arr, x, y);
 		LinkedList<Integer> zeroes = indexesOf(quad, 0);
-		Collections.shuffle(zeroes);
-		int i = zeroes.pop();
-		int j = zeroes.pop();
-		int t = quadNew[i];
-		quadNew[i] = quadNew[j];
-		quadNew[j] = t;
-		setQuad(quadNew, arr, x, y);
-	}
-
-	private static int[][] solve(final int[][] board) {
-		
-		int[][] newSolution = setupSudoku(board);
-		int[][] bestSolution = newSolution;
-		
-		int numErrors = countErrors(newSolution);
-		int bestErrors = numErrors;
-		double temp = 0.5;
-		double coolingRate = 0.99999;
-		long count =0;
-		
-		System.out.println("Starting errors " + numErrors);
-		while (count < 40000) {
-			if(count %1000 == 0) {
-			//	System.out.printf("Iteration: %d score=%d temp=%.5f best=%d%n",count,numErrors,temp,bestErrors);
-			}
-			int[][] candidateSolution = ArrayUtils.clone(newSolution);
-			swap(candidateSolution, board, rand.nextInt(3), rand.nextInt(3));
-			int candidateErrors = countErrors(candidateSolution);
-
-			//if (acceptanceProbability(numErrors, newNumErrors, temp) > rand.nextDouble()) {
-			if(Math.exp(((float)(numErrors-candidateErrors))/temp) - rand.nextDouble() > 0) {
-				newSolution = candidateSolution;
-				numErrors = candidateErrors;
-				// System.out.println("new errors =" + newNumErrors);
-			}
-			if(candidateErrors < bestErrors) {
-				bestErrors = candidateErrors;
-				bestSolution  = ArrayUtils.clone(newSolution);
-			}
-			if (numErrors == 0) {
+		while(true) {
+			if(zeroes.size() >= 2) {
+				Collections.shuffle(zeroes);
+				int i = zeroes.pop();
+				int j = zeroes.pop();
+				
+				// check if this swap is a valid one
+				// given the candidates
+				int[] candsij = cands[x*3 + i/3][y*3 + j%3];
+				if(candsij[i] == 0 || candsij[j]==0) {
+					int t = quadNew[i];
+					quadNew[i] = quadNew[j];
+					quadNew[j] = t;
+					setQuad(quadNew, arr, x, y);
+					break;
+				}
+			} else {
 				break;
 			}
-			temp *= coolingRate;
-			count++;
 		}
-		System.out.println("Solved in "+count+" iterations");
-		return bestSolution;
 	}
 
-	public static void main(String[] args) throws FileNotFoundException {
+	private static int[][] solve(final int[][] board,String grid) {
+		boolean solved = false;
+		int retries = 0;
+		int[][][] cands =  candidates(board);
+		while(retries < 200) {
+			int[][] newSolution = setupSudoku(board);	
+			int[][] bestSolution = arrayCopy2D(newSolution);
+			int numErrors = countErrors(newSolution);
+			if(numErrors ==0) {
+				System.out.println(grid+ " Solved by deduction");
+				return board;
+			}
+			int bestErrors = numErrors;
+			double temp = 0.4 + retries/1000d;
+			double coolingRate = 0.9999;
+			long count = 0;
+			while (count < 500000) {
+				int[][] candidateSolution = arrayCopy2D(newSolution);
+				swap(candidateSolution, board,cands, rand.nextInt(3), rand.nextInt(3));
+				int candidateErrors = countErrors(candidateSolution);
+				double scoreDelta = (double)(numErrors - candidateErrors);
+				
+				if(Math.exp(scoreDelta/temp) - rand.nextDouble() > 0) {
+					newSolution = arrayCopy2D(candidateSolution);
+					numErrors = candidateErrors;
+				}
+				if(candidateErrors < bestErrors) {		
+					bestSolution  = arrayCopy2D(newSolution);
+					bestErrors = countErrors(bestSolution);
+				}
+				if (numErrors == 0) {
+					newSolution = candidateSolution;
+					System.out.println(grid + " Solved in "+count+" iterations and "+ retries + " retries");
+					solved = true;
+					return newSolution;
+				}
+				temp *= coolingRate;
+				count++;
+			}
+			if(!solved)
+				retries++;
+			else 
+				break;
+		}
+		if(!solved)
+			System.out.println(grid+ " failed in "+retries+" retries");
+		return board;
+	}
+	private static int[][] arrayCopy2D(final int[][] arr) {
+		int[][] copy = new int[arr.length][arr[0].length];
+		for (int i = 0; i < arr.length; i++) {
+			System.arraycopy(arr[i], 0, copy[i], 0, arr[i].length);
+		}
+		return copy;
+	}
+	private static void printArr(int[][] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(Arrays.toString(arr[i]));
+		}
+		System.out.println("--------");
+	}
+	private static class Solver implements Runnable {
+		private int[][] board;
+		private String grid;
+		public Solver(int[][] board,String grid) {
+			this.board = board;
+			this.grid = grid;
+		}
+		@Override
+		public void run() {
+			board = solve(board,grid);
+			sum.addAndGet(board[0][0] * 100 + board[0][1] * 10 + board[0][2]);
+			printArr(board);
+		}
+		
+	}
+	public static void main(String[] args) throws FileNotFoundException, InterruptedException {
 		Scanner s = new Scanner(new File("p096_sudoku.txt"));
-		int sum = 0;
+		ExecutorService exec = Executors.newWorkStealingPool();
 		long started = System.nanoTime();
-		for (int i = 0; i < 1; i++) {
-			System.out.println(s.nextLine());
+		for (int i = 0; i < 50; i++) {
+			String grid = s.nextLine();
 			int[][] board = new int[9][9];
 			for (int j = 0; j < 9; j++) {
 				char[] line = s.nextLine().toCharArray();
@@ -197,17 +258,13 @@ public class Prob96 {
 					board[j][k] = Integer.parseInt(Character.toString(line[k]));
 				}
 			}
-			board = solve(board);
-			System.out.println("Final Errors: " + countErrors(board));
-			sum += board[0][0] * 100 + board[0][1] * 10 + board[0][2];
-			for (int j = 0; j < board.length; j++) {
-				System.out.println(Arrays.toString(board[j]));
-			}
+			exec.execute(new Solver(board,grid));
 		}
+		exec.shutdown();
+		exec.awaitTermination(Long.MAX_VALUE, TimeUnit.DAYS);
 		s.close();
 		System.out.println(sum);
 		long fin = System.nanoTime();
-
 		System.out.println("Took: " + ((fin - started) / 1000000000f) + "s");
 	}
 
